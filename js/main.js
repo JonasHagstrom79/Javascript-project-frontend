9eb586cd43ef1e494b0683c1d669d0cf5c1d556f
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2022-12-18 16:05:23 +0100

    adapting atlas to persons

---
+++ js/main.js
@@ -0,0 +1,284 @@
+import { Atlas } from './atlas.js';
+import { RESTDataSource } from './rest-data-source.js';
+
+/** The data source for our Atlas */
+//const dataSource = new RESTDataSource("https://caha1906-lab1-backend-dt190g.azurewebsites.net/");
+const dataSource = new RESTDataSource("http://localhost:3000");
+
+/** The Atlas instance */
+const atlas = new Atlas(dataSource);
+
+/** The name of the page displaying the list of My courses */
+const MY_COURSES_PAGE = "my-courses.html";
+
+/** The name of the page curretly beeing displayed */
+let currentPage = "index.html";
+
+/** An array of all persons to list on the page. */
+let persons = [];
+
+let courses = []; //TODO:to get rid of referene-error
+
+/** Limit the number of persons to show on the page. */
+const limit = 100;
+
+/** Compare two course codes (can be used when sorting a list of courses) */
+const courseCodeDescending = (a, b) => a.courseCode > b.courseCode ? 1 : -1;
+
+
+
+/**
+ * Handles initialization of the app.
+ */
+function starterFunction() {
+	// Get the name of the current page
+	currentPage = window.location.pathname.split("/").find(str => str.includes(".html"));
+
+	
+	
+	// Get the list of courses from Atlas
+	const personPromise = currentPage == MY_COURSES_PAGE ? atlas.getPersons() : atlas.getPersons()//: atlas.getCourses();
+	console.log("PersonPromise"); //TODO:remove
+    console.log(personPromise) //TODO:remove
+    console.log("PersonPromise"); //TODO:remove
+	personPromise
+	.then(async fetchedPersons => {
+		persons = await fetchedPersons.json();
+		console.log(persons) //TODO:remove		
+		createTable(); // create the table with the fetched courses
+	})
+	.catch(error => console.error(`An error occurd when getting persons from Atlas: ${error}`));
+}
+
+/**
+* Creates the HTML table displaying the courses (either Miun courses or My courses).
+*/
+function createTable() {
+	// The type of course (a Miun course or a My course) to be listed in the table depends
+	// on the name of the current location (name of the page). The structure of the table 
+	// displaying course data also depends upon this.
+
+	// Descide the function to be used when creaing the HTML tabel
+	const tableCreator = currentPage == MY_COURSES_PAGE ? createTableForPersons : createTableForPersons//TODO: to get it right//createTableForMiunCourses;
+
+	// Regardles the type of table to create, sort and filter the courses
+	const searchString = document.getElementById("search").value.toLowerCase();
+
+	// Keep the original course array intact by assigning the filterad courses to a new array
+	// let coursesToList = courses.filter(course => searchFilter(course, searchString));
+	// coursesToList = coursesToList.sort(courseCodeDescending).slice(0, limit); // limit the number of courses to display
+
+    // Keep the original course array intact by assigning the filterad courses to a new array
+    let personsToList = persons//.filter(person => searchFilter(person, searchString));
+	personsToList = personsToList.sort(courseCodeDescending).slice(0, limit); // limit the number of courses to display
+
+	// Clear any existing data in the table
+	const table = document.getElementById("courses_table");
+	table.innerHTML = null;
+	
+	// Create the table 
+	// (a call to createTableForMyCourses(courses, table) or createTableForMiunCourses(courses, table))
+	tableCreator(personsToList, table);
+}
+
+/**
+* Create table rows for all Miun courses in the array.
+* @param courses an array of Miun courses to create table rows for
+* @param table the table or the table body to add the rows to
+*/
+function createTableForMiunCourses(persons, table) {
+	// For each course create a table row with course data
+	persons.forEach(person => {
+		// Make a table row
+		const tr = document.createElement("tr");
+
+		// Populate the row with the data to display
+		createTd(course.courseCode, tr); //TODO: ad person. here?
+		createTd(course.name, tr);		
+		createTd(course.subjectCode, tr)
+		createTd(course.progression, tr);
+		createTd(course.points, tr);
+		createTd(course.institutionCode, tr,
+			element => element.classList.add("center"));
+
+		// Add the row to the table
+		table.appendChild(tr);
+	});
+}
+
+/**
+* Create table rows for all My courses in the array.
+* @param persons an array of persons to create table rows for
+* @param table the table or the table body to add the rows to
+*/
+async function createTableForPersons(persons, table) {
+	// Get grades from Atlas and then create the table
+	//const grades = await atlas.getGrades().then(grades => grades.json())
+	
+	// For each My course create a table row with course data
+	persons.forEach(person => {
+		// Make a table row
+		const tr = document.createElement("tr");
+
+		// Populate the row with the data to display
+		createTd(person.firstName, tr);
+		createTd(person.surName, tr); //TODO:här!!!
+			
+		// Create a td to hold the select element for selecting grade
+		const td = document.createElement("td");
+		td.classList.add("center");
+
+		// Create a select element for the grades that can be selected
+		// const selectElement = document.createElement("select");
+		// selectElement.id = "select_" + course.courseCode;
+			
+		// Add each grade as an option in the select element and set
+		// the course grade as the selected grade in the list
+		// createGradeOptions(selectElement, grades, course.grade);
+		
+		// Eventlistenser to select option //TODO: Here for change phone-number!!
+		//selectElement.addEventListener('change', _event => updateMyCourse(course.courseCode));
+
+		//td.appendChild(selectElement);
+		tr.appendChild(td);
+
+		// Add delete-button to the row
+		const _td = document.createElement('td');
+		const btnDelete = document.createElement('button');
+		btnDelete.innerText = 'Radera';
+		btnDelete.socialSecurityNumber = person.socialSecurityNumber;
+		// Add listener to the button
+		btnDelete.addEventListener('click', _event => deletePerson(person.socialSecurityNumber));
+		_td.appendChild(btnDelete);
+		tr.appendChild(_td);
+
+		// Add the row to the table
+		table.appendChild(tr);		
+		
+	});
+
+	// Creates gradeoptions for (to be)added course	
+	const select = document.getElementById('newMyCourseSelect');	
+	createGradeOptions(select, grades, grades); 
+
+	// Click event to submit button in the form
+	if (currentPage.toLocaleLowerCase() == MY_COURSES_PAGE.toLocaleLowerCase()) {
+
+		//document.querySelector('#newMyCourseSubmit').addEventListener('click', addNewMyCourse);
+        document.querySelector('#newPersonSubmit').addEventListener('click', addNewMyCourse);
+
+	};
+	
+}
+
+/**
+ * Adds a new course to myCourses
+ */
+async function addNewMyCourse() {
+	
+	// Gets the data fron the html-form
+	const form = document.querySelector('#newMyCourse');
+	const formBody = new FormData(form);
+	
+	await atlas.addMyCourse(formBody.get('courseCode'), formBody.get('grade')).then(res => res.json());
+	
+	// Refreshes the page
+	location.reload();
+	form.reset();	
+}
+
+/**
+ *  Updates a course grade with selected grade
+ */
+async function updateMyCourse(e) {
+
+	// Gets the coursecode 
+	const code = e
+
+	// Gets the grade
+	const grade = document.getElementById(`select_`+code); //NYTT2022-11-29
+	
+	// Gets the value from select element select option
+	const value = grade.value	
+
+	// Upddates course grade
+	const updateMyCourse = await atlas.updateMyCourse(e, value).then(res => res.json());
+	const updateMyCourseIndex = courses.findIndex(obj => obj.courseCode == e); 
+	
+	courses[updateMyCourseIndex] = updateMyCourse;
+
+}
+
+/**
+ * Deletes a Person
+ */
+async function deletePerson(e) {
+	
+	// Deletes the course from the event e
+	const deletedPerson = await atlas.deletePerson(e).then(res => res.json());
+	
+	// Returns the courses
+	const result = persons.filter(person => person.socialSecurityNumber !== deletedPerson.socialSecurityNumber);
+	
+	// Refreshes the page
+	location.reload();
+	
+	return result;	
+}
+
+/**
+* Create option elements for the specified select element.
+* @param selectElement the select element to create and add option elements for
+* @param grades an array of grades to create option elements for
+* @param selectedGrade the grade to be the selected option in the selectElement
+*/
+function createGradeOptions(selectElement, grades, selectedGrade) {
+	grades = ["-","fx","f","e","d","c","b","a"];  //TODO:remove!		
+	// For each grade 
+	for(let grade of grades) {
+		
+		// Create an option element
+		const option = document.createElement("option");
+		console.log("grade: ") //TODO:remove!
+		console.log(grade) //TODO:remove!
+		
+		// Add the text of the grade to the option
+		option.innerText = grade.toUpperCase(); //TODO:blir fel här med uppercase
+		
+		// Add the option the selectElement
+		selectElement.appendChild(option)
+		
+	}	
+	console.log("grades: ")
+	console.log(grades)
+	// Set selectedGrade from myCourses
+	selectElement.value = selectedGrade	
+	
+}
+
+/**
+* Create a data cell (td element) with the specified text
+* @param text the text to to be displayed in the data cell
+* @param tr the table row to add the data cell to
+* @param extra a lambda that handles any extra that needs to be added to the data cell
+*/
+function createTd(text, tr, extra) {
+	const td = document.createElement("td");
+	td.innerText = text;
+	
+	if (extra) {
+		extra(td);
+	}
+
+	tr.appendChild(td);
+}
+
+/**
+ * Perform a search for courses matching the text entered in the search input.
+ */
+function searchCourses() {
+	// A re-creation of the table will filter out the courses not matching the searched value
+	createTable();
+}
+
+document.addEventListener('DOMContentLoaded', starterFunction);
---
+++ js/rest-data-source.js
@@ -0,0 +1,121 @@
+import { DataSource } from "./data-source.js";
+
+
+/**
+ * This class represents a data source where a REST API is used 
+ * to get data from.
+ * @extends DataSource
+ */
+export class RESTDataSource extends DataSource {
+	/**
+	 * Create a new data source with the specified JSON file as its source of data.
+	 * @param url the name of the JSON file to be used as source of data
+	 */
+	constructor(url) {
+		super(url);
+	}
+
+	/**
+	 * Get data from the specified endpoint.
+	 * @param endpoint default an empty string ''
+	 * @param method default GET
+	 * @param body default an empty body {}
+	 * @return a Promise that resolves to the value of the given endpoint
+	 */
+	 async getData(endpoint = '', method = 'GET', body = {}) {
+		return super.getData(endpoint, method, body);
+		
+	}
+
+    /**
+	* Get all persons from the REST API.
+	* @return a Promise that resolves to an array of courses
+	*/
+	async getPersons() { //getCourses
+
+		return this.getData('/api/persons');
+
+	}
+
+	/**
+	* Get the person with specific social security number from the REST API.
+	* @param socialSecurityNumber the course code of the course to get
+	* @return a Promise that resolves to a course object or {} if the course doesn't exist
+	*/
+	async getPerson(socialSecurityNumber) { //getCourse		
+		
+		return this.getData('/api/persons' + socialSecurityNumber);
+
+	}
+
+	/**
+	* Get all My courses from the REST API.
+	* @return a Promise that resolves to an array of My courses (a Miun course with grade included)
+	*/
+	async getMyCourses() { //TODO:remove?
+		
+		return this.getData('/api/courses/my');
+		
+	}
+
+	/**
+	* Get the My course with the specified course code from the REST API.
+	* @param courseCode the course code of the course to get
+	* @return a Promise that resolves to a My course object or {} if the course doesn't exist
+	*/
+	async getMyCourse(courseCode) { //TODO:remove?	 
+
+		return this.getData('/api/courses/my/:'+courseCode);
+		
+	}
+
+	/**
+	* Add a My course with a grade to the REST API.
+	* @param courseCode the course code for the course to be added
+	* @param grade the student's grade in the course being added
+	* @return a Promise that resolves to a My course object of the course added or an error 
+	*         message explaining why the course couldn't be added
+	*/
+	async addPerson(firstName, surName, address, socialSecurityNumber, phone) { //addCourse
+		
+		return this.getData('/api/persons', 'POST', 
+		//{'courseCode': courseCode, 'grade': grade});
+        {'firstName': firstName, 'surName':surName, 'address':address, 'socialSecurityNumber':socialSecurityNumber, 'phone':phone});
+	}	
+
+	/**
+	* Delete a person from the REST API.
+	* @param socialSecurityNumber the course code for the course to be deleted
+	* @return a Promise that resolves to the My course deleted or an error 
+	*         message explaining why the course couldn't be deleted
+	*/
+	async deletePerson(socialSecurityNumber) { //deleteMyCourse
+		
+		return this.getData('/api/persons' + socialSecurityNumber, 'DELETE');
+		
+	}
+
+	/**
+	* Update the grade for a My course in the REST API.
+	* @param  socialSecurityNumber the id for the person to update the phone for
+	* @param phone the new phone number
+	* @return a Promise that resolves to the My course updated or an error 
+	*         message explaining why the course's grade couldn't be updated
+	*/
+	updatePerson(socialSecurityNumber, phone) { //updateMyCourse
+		
+		return this.getData('/api/persons/' +socialSecurityNumber, 'PUT',
+		{'socialSecurityNumber': socialSecurityNumber, 'phone': phone});
+
+	}
+
+	/**
+	* Get all grades in the grade scale from the REST API.
+	* @return a Promise that resolves to an array of grades
+	*/
+	async getGrades() { //TODO:remove?
+		
+		return this.getData('/api/grades');
+
+	}
+}
